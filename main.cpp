ea1e846569dfe337aa69dc5d82ac28586c9069f8
Farnaz Mozhgani <96700080+farnazi2000@users.noreply.github.com>
2023-04-25 13:06:28 -0700

    Done with parser function, parsing error detection

--- main.cpp
+++ main.cpp
@@ -1,8 +1,33 @@
-#include "rapidjson/include/rapidjson/document.h"
 #include <iostream>
+#include "parser.h"
 using namespace std;
-
 int main() {
+    Parser p;
+    int choice; 
 
-    return 0;
+  do {
+    cout << "Please pick from the menu options\n";
+    cout << "1 - Check for reliability of your file: \n";
+    cout << "2 - Read/Parse Json file: \n";
+    cin >> choice;
+    switch (choice)
+    {
+    case 1: 
+        cout<< "Check for Parsing error ......\n";
+        p.parseError();
+        break;
+    
+    case 2:
+        cout << "Reading .....\n";
+        p.parseJson();
+        break;
+    
+    default:
+    cout << "invalid choice\n";
+    
+    }
+
+}while (choice !=3);
+
+return 0;
 }
