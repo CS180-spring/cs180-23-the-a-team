a6778559189b8e0ba954d038e3f86ba284c8ae59
EndUser <enduser@EndUsers-MacBook-Pro-2.local>
2023-04-24 18:13:37 -0700

    Added Gui instance to show the interface

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include "rapidjson/include/rapidjson/document.h"
 #include <iostream>
+#include "src/gui.h"
+
 using namespace std;
 
 int main() {
 
+     gui Gui;
+     Gui.main();
     return 0;
 }
