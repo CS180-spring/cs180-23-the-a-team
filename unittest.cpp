cdfcd2f78f276608f62849ad8284ac196c44113c
muneebghori <muneebghori99@gmail.com>
2023-05-09 16:45:23 -0700

    Added google test for parseError function in json.cpp

--- unittest.cpp
+++ unittest.cpp
@@ -23,6 +23,28 @@ TEST(csvtojson, withextension){
 }
 */
 
+TEST(JsonTest, ParseErrorTest) {
+  // Create a file with a JSON syntax error
+  std::ofstream file("data.json");
+  file << "{ 'name': 'John', 'age': 30, }"; // Syntax error: trailing comma
+  file.close();
+
+  // Call the function being tested
+  Json json;
+  json.parseError();
+
+  // Check that the function output contains the expected error message
+  testing::internal::CaptureStdout();
+  json.parseError();
+  std::string output = testing::internal::GetCapturedStdout();
+  EXPECT_TRUE(output.find("Error(offset 24):") != std::string::npos);
+  EXPECT_TRUE(output.find("trailing comma") != std::string::npos);
+
+  // Remove the test file
+  std::remove("data.json");
+}
+
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
