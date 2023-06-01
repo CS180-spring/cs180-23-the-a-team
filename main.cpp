fdd20d93e26001daedc7842699f38f12a95b173c
Farnaz Mozhgani <fmozh001@xe-01.cs.ucr.edu>
2023-05-31 22:30:31 -0700

    removed unnecessary files

--- main.cpp
+++ main.cpp
@@ -23,41 +23,7 @@ int main()
      Gui.main();
     return 0;
 }
-/*
-#include <iostream>
-#include "parser.h"
-using namespace std;
-int main() {
-    Parser p;
-    int choice; 
 
-  do {
-    cout << "Please pick from the menu options\n";
-    cout << "1 - Check for reliability of your file: \n";
-    cout << "2 - Read/Parse Json file: \n";
-    cin >> choice;
-    switch (choice)
-    {
-    case 1: 
-        cout<< "Check for Parsing error ......\n";
-        p.parseError();
-        break;
-    
-    case 2:
-        cout << "Reading .....\n";
-        p.parseJson();
-        break;
-    
-    default:
-    cout << "invalid choice\n";
-    
-    }
-
-}while (choice !=3);
-
-return 0;
-}
-*/
 /*
     csvtojson c;
     string name;
