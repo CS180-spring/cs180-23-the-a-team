9b24ac9e5ba2321e52a0232358890d015b08adc3
Harsh Patel <hp6164@hotmail.com>
2023-05-31 22:40:45 -0700

    deleting testing code in main

--- main.cpp
+++ main.cpp
@@ -12,32 +12,10 @@ using namespace std;
 
 int main() 
 {
-    /*
-    Collection c("this3");
-    c.AddCollection("this3");
-    c.AddCollection("this4");
-    c.DeleteCollection("this3");
-    c.ViewAllCollections();
-    */
+
      gui Gui;
      Gui.main();
     return 0;
 }
 
-/*
-    csvtojson c;
-    string name;
-    int sizeofdata;
-    stringstream outputofcsv;
 
-    cout << "PLEASE input csv file to ..[ProjectFolder]/Inputs/" << endl;
-    cout << "Enter the name of your csv file(no spaces): ";
-    cin >> name;
-    cout << "Enter the amount of rows you wish to import(excluding row1 with column names): ";
-    cin >> sizeofdata;
-    c.printcsv(name, sizeofdata, outputofcsv);
-    outputofcsv.str();
-    
-    return 0;
-}
-*/
